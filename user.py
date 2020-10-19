ab1810d8a4c9dbdfea39a05d00ace412309ad038
omid esmaeili <omidesmaeeli4@gmail.com>
2020-10-19 22:25:17 +0330

    Add tutorial

--- user.py
+++ user.py
@@ -1,35 +1,40 @@
-username_in_database = [
-    {
-        "username": "omid",
-        "password": "123"
-    },
-    {
-        "username": "amin",
-        "password": "123456"
-    },
-    {
-        "username": "parvin",
-        "password": "98765"
-    },
-    {
-        "username": "nikkhah",
-        "password": "56789"
-    }
-]
+class User:
+    count_users = 0
+
+    def __init__(self, first_name, last_name, role):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.role = role
+        # self.add_count()
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def is_customer(self):
+        return True if self.role == "customer" else False
+
+    @classmethod
+    def add_count(cls):
+        cls.count_users += 1
 
 
-def is_login(username: str = None, password: str = None) -> bool:
-    if username is None or password is None:
-        return False
-    else:
-        for user in username_in_database:
-            if username == user.get("username") and password == user.get("password"):
-                return True
-        return False
+user_1_obj = User("omid", "esmaeili", "customer")
+user_2_obj = User("hasan", "hasssss", "manager")
 
+print(user_1_obj.full_name)
 
-a = is_login("parvin", "98765")
-print(a)
-
-
-print("new changed")
+# print(user_1_obj.first_name)
+# print(user_1_obj.last_name)
+# print(user_1_obj.role)
+# print(user_1_obj.count_users)
+# print(user_2_obj.count_users)
+# print(user_1_obj.is_customer())
+# print(user_2_obj.is_customer())
+# User.add_count()
+# User.add_count()
+# User.add_count()
+# User.add_count()
+# print(User.count_users)
+print(user_2_obj.count_users)
+print(user_1_obj.count_users)
