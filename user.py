aaf9a1beb642b4b656e8070272612ae434549b6f
omid esmaeili <omidesmaeeli4@gmail.com>
2020-10-21 20:48:16 +0330

    Add user

--- user.py
+++ user.py
@@ -1,11 +1,13 @@
 class User:
-    count_users = 0
+    # count_users = 0
 
     def __init__(self, first_name, last_name, role):
         self.first_name = first_name
         self.last_name = last_name
         self.role = role
+        self.count_users = 0
         # self.add_count()
+        # self.count_users = 0
 
     @property
     def full_name(self):
@@ -14,15 +16,28 @@ class User:
     def is_customer(self):
         return True if self.role == "customer" else False
 
-    @classmethod
-    def add_count(cls):
-        cls.count_users += 1
+    # @classmethod
+    # def add_count(cls):
+    #     cls.count_users += 1
+
+    def add_count(self):
+        self.count_users += 1
 
 
 user_1_obj = User("omid", "esmaeili", "customer")
 user_2_obj = User("hasan", "hasssss", "manager")
 
-print(user_1_obj.full_name)
+print(111, user_1_obj.count_users)
+print(222, user_2_obj.count_users)
+
+user_1_obj.add_count()
+user_1_obj.add_count()
+user_1_obj.add_count()
+
+print(333, user_1_obj.count_users)
+print(444, user_2_obj.count_users)
+
+
 
 # print(user_1_obj.first_name)
 # print(user_1_obj.last_name)
@@ -36,5 +51,5 @@ print(user_1_obj.full_name)
 # User.add_count()
 # User.add_count()
 # print(User.count_users)
-print(user_2_obj.count_users)
-print(user_1_obj.count_users)
+# print(user_2_obj.count_users)
+# print(user_1_obj.count_users)
