4070e4098ec34429594afe139480254d0f163347
omid esmaeili <omidesmaeeli4@gmail.com>
2020-10-21 20:49:31 +0330

    Add product and show_menu

--- product.py
+++ product.py
@@ -1,32 +1,54 @@
+import itertools
+from category import Category
+
+
 class Product:
-    __products_db = []
+    __db = []
+    __id = itertools.count(1)
 
-    def __init__(self, name, price, category, off):
-        self.name = name
-        self.price = price
-        self.category = category
-        self.off = off
-
-    def add_product(self):
-        self.__products_db.append(self)
+    def save(self, name=None, price=None, category=None, off=None):
+        ctg = Category().exits(category)
+        if ctg:
+            g = {
+                "id": next(Product.__id),
+                "name": name,
+                "price": int(price),
+                "category_id": ctg.get("id"),
+                "off": float(off)
+            }
+            self.__db.append(g)
+            return True
+        else:
+            return False
 
     @classmethod
     def get_products_db(cls):
         # print(cls.products_db)
-        return cls.__products_db
+        return cls.__db
 
     def __repr__(self):
         return self.name
 
+    def delete(self, id):
+        for product in self.__db:
+            if id == product.__id:
+                self.__db.remove(product)
 
-shoes_product_obj = Product("shoes", 200000, "cloths", 23)
-shoes_product_obj.add_product()
+    # def update(self, id):
+    #     for product in self.__db:
+    #         if id == product.__id:
+    #             if name:
+    #                 product.name = name
+
+# shoes_product_obj = Product("shoes", 200000, "cloths", 23)
+# shoes_product_obj.add_product()
 # shoes_product_obj.show()
 
-bag_product_obj = Product("bag", 180000, "cloths", 2.1)
-bag_product_obj.add_product()
+# bag_product_obj = Product("bag", 180000, "cloths", 2.1)
+# bag_product_obj.add_product()
 # bag_product_obj.show()
-
+# print(shoes_product_obj.id)
+# print(bag_product_obj.id)
 
 # products = Product.get_products_db()
 #
@@ -37,3 +59,12 @@ bag_product_obj.add_product()
 
 
 # print(Product.__products_db)
+
+# g = itertools.count(1, 2)
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
--- show_menu.py
+++ show_menu.py
@@ -1,10 +1,15 @@
+from product import Product
+from category import Category
+
 a = {
-    "1": "Add product",
+    "1": "Add Category",
     "2": "Update product",
     "3": "Delete product",
-    "4": "Retrieve products",
+    "4": "Retrieve category",
     "5": "Add Category",
     "6": "Retrieve categories",
+    "7": "Add product",
+    "8": "get product"
 }
 
 print("---------- Welcome ----------")
@@ -13,3 +18,30 @@ for num, title in a.items():
 
 print("*****************************")
 user_input = input("Enter your number:")
+print(user_input)
+while True:
+
+    if user_input == "1":
+        name = input("name: ")
+        if Category().save(name):
+            print(f"success add category: {name}")
+        else:
+            print(f"This category exists with {name}")
+
+    if user_input == "4":
+        print(Category().get_all_category())
+
+    if user_input == "7":
+        name = input("name: ")
+        price = input("price: ")
+        category = input("category: ")
+        off = input("off: ")
+        res = Product().save(name, price, category, off)
+        if res:
+            print("yesssss")
+        else:
+            print(f"Please add category with {category}")
+    if user_input == "8":
+        print(Product().get_products_db())
+
+    user_input = input("Enter your number:")
